d1d13884d44281b1f9cf141c09c818dd9f852b5a
econquer <econquer@toss.im>
2021-03-25 18:12:01 +0900

    feat: add metrics custom tags function

--- cache-core/src/main/kotlin/im/toss/util/cache/Cache.kt
+++ cache-core/src/main/kotlin/im/toss/util/cache/Cache.kt
@@ -1,7 +1,14 @@
 package im.toss.util.cache
 
+import im.toss.util.cache.metrics.CacheMetrics
+import io.micrometer.core.instrument.Tag
+
 interface Cache {
     data class KeyFunction(
         val function: (name: String, key: Any) -> String
+    )
+
+    data class CustomTagsFunction(
+        val function: (namespaceId: String, cacheMetrics: CacheMetrics) -> List<Tag>
     )
 }
